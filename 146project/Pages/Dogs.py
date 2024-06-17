72e2560fa6d98fac6707ea1a174184fe236d623d
yuvaldror <yuval.dror@campus.technion.ac.il>
2024-06-17 19:42:45 +0300

    Update Dogs.py

--- 146project/Pages/Dogs.py
+++ 146project/Pages/Dogs.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 from datetime import datetime
-from streamlit_option_menu import option_menu
+
 
 if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
     st.error("לא ניתן לגשת לעמוד ללא התחברות")
