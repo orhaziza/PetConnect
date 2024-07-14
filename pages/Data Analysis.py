d659af03b3b5330881028c3c58e6e9ce5cd69776
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-07-14 14:36:20 +0300

    Update Data Analysis.py

--- pages/Data Analysis.py
+++ pages/Data Analysis.py
@@ -5,9 +5,23 @@ from datetime import datetime
 from streamlit_option_menu import option_menu
 import matplotlib.pyplot as plt
 
-# application_df, adopter_df, dogs_df, Foster_Home_df
+def plot_Applications(application_df):
+    platform_counts = df['SourcePlatform'].value_counts()
+    # plt.figure(figsize=(8, 8))
+    # plt.pie(platform_counts, labels=platform_counts.index, autopct='%1.1f%%', startangle=140)
+    fig, ax = plt.subplots()
+    ax.pie(platform_counts, labels=platform_counts.index, autopct='%1.1f%%', startangle=140)
+    ax.set_title('Distribution of Requests by Platform')
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    st.pyplot(fig)
+    
+def show_data_analysis_page():
+    st.set_page_config(page_title='Data Analysis', layout='wide')
 
-def load_df():
+    if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
+        st.error("לא ניתן לגשת לעמוד ללא התחברות")
+        st.stop()
+    
     application_file_path = "Data/AdoptionApplication.csv"
     if not os.path.exists(application_file_path):
         st.error("applications file does not exist.")
@@ -31,38 +45,10 @@ def load_df():
         st.error("Foster Home file does not exist.")
         st.stop()
     Foster_Home_df = pd.read_csv(dogs_file_path, encoding='Windows-1255')
-
-def plot_Applications():
-    platform_counts = df['SourcePlatform'].value_counts()
-    # plt.figure(figsize=(8, 8))
-    # plt.pie(platform_counts, labels=platform_counts.index, autopct='%1.1f%%', startangle=140)
-    fig, ax = plt.subplots()
-    ax.pie(platform_counts, labels=platform_counts.index, autopct='%1.1f%%', startangle=140)
-    ax.set_title('Distribution of Requests by Platform')
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    st.pyplot(fig)
-
-def plot_Applications():
-    platform_counts = df['SourcePlatform'].value_counts()
-    # plt.figure(figsize=(8, 8))
-    # plt.pie(platform_counts, labels=platform_counts.index, autopct='%1.1f%%', startangle=140)
-    fig, ax = plt.subplots()
-    ax.pie(platform_counts, labels=platform_counts.index, autopct='%1.1f%%', startangle=140)
-    ax.set_title('Distribution of Requests by Platform')
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    st.pyplot(fig)
     
-def show_data_analysis_page():
-    st.set_page_config(page_title='Data Analysis', layout='wide')
-
-    if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
-        st.error("לא ניתן לגשת לעמוד ללא התחברות")
-        st.stop()
-    
-    load_df()
     with st.container():
         col1 , col2 = st.columns([1,1], gap="small")
         with col1:
-            plot_Applications()
+            plot_Applications(application_df)
 
 show_data_analysis_page()
