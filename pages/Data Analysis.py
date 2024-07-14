6997836b51298ad8c3683852a508ad470cbafaab
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-07-14 15:23:44 +0300

    Update Data Analysis.py

--- pages/Data Analysis.py
+++ pages/Data Analysis.py
@@ -24,20 +24,20 @@ def plot_Dogs(dogs_df):
     lst = ['Age', 'breed size', 'gender', 'vaccine_1', 'vaccine_2', 'isSpay', 'childrenFirendly', 'animalFirendly', 'healthStatus', 'energylevel', 'photographStatus', 'adoptionStatus', 'pottyTrained']
     characteristic = st.selectbox('בחר מאפיין', lst)
 
-  if dogs_df[characteristic].dtype == 'bool':
+    if dogs_df[characteristic].dtype == 'bool':
       # Convert boolean to string
       distribution = dogs_df[characteristic].astype(str).value_counts()
       
-  elif pd.api.types.is_numeric_dtype(dogs_df[characteristic]):
+    elif pd.api.types.is_numeric_dtype(dogs_df[characteristic]):
         # Create bins for numerical columns
         bins = st.slider('Select number of bins for numerical data', min_value=2, max_value=20, value=5)
         distribution = pd.cut(dogs_df[characteristic], bins=bins).value_counts().sort_index()
-  else:
+    else:
         distribution = dogs_df[characteristic].value_counts()
 
-# Display the bar chart using st.bar_chart
-st.subheader(f'Distribution of Dogs by {column.capitalize()}')
-st.bar_chart(distribution)
+    # Display the bar chart using st.bar_chart
+    st.subheader(f'Distribution of Dogs by {column.capitalize()}')
+    st.bar_chart(distribution)
 
     
 def show_data_analysis_page():
