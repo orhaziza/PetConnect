6bc5f16751711af77b2e9a77894f0be58b9dcfb5
yuvaldror <yuval.dror@campus.technion.ac.il>
2024-07-21 14:09:08 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import bycript
+import bcript
 # Set up the page configuration
 st.set_page_config(page_title='פט קונקט', layout='wide', page_icon = 'Data/Logo.png' )
 
