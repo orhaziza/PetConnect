39d69e83a58c09ec3e5354c60b3985e4b8faa449
yuvaldror <yuval.dror@campus.technion.ac.il>
2024-07-21 14:32:00 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -29,7 +29,8 @@ def login(username, password):
 
     users = load_users()
     user = users[users['username'] == username]
-    
+    st.markdown(username)
+    st.mardown(user)
     if not user.empty:
         stored_password = user.iloc[0]['password']
         if check_password(password, stored_password.encode()):
