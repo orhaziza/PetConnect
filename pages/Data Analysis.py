2e53dd83dd9fc2ff3fccf32b97a954988ce534f7
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-07-28 17:14:50 +0300

    Update Data Analysis.py

--- pages/Data Analysis.py
+++ pages/Data Analysis.py
@@ -17,6 +17,7 @@ with con1:
 with col1:
     st.markdown("<h1 style='text-align: center;'>Data Analysis Page</h1>", unsafe_allow_html=True)
     st.markdown("<h3 style='text-align: center;'>כאן תוכלו לצפות בויזואליזציות על בסיס הנתונים שנאספו עד כה</h3>", unsafe_allow_html=True)
+    st.button("רענן מידע")
 with col2:
     st.image("Data/Logo.png", width=100)
 
