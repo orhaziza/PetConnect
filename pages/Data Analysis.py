2cbf9d5e552b2a384d4890dc5f11a0b866602637
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-07-28 17:41:55 +0300

    Update Data Analysis.py

--- pages/Data Analysis.py
+++ pages/Data Analysis.py
@@ -22,8 +22,10 @@ with st.container():
     with col1:
         st.markdown("<h1 style='background-color:#42f598; text-align: center;'>Data Analysis Page</h1>", unsafe_allow_html=True)
         st.markdown("<h3 style='background-color:#0066cc; text-align: center;'>כאן תוכלו לצפות בויזואליזציות על בסיס הנתונים שנאספו עד כה</h3>", unsafe_allow_html=True)
-        if st.button("רענן מידע", use_container_width=True):
-            st.cache_data.clear()
+        col3, col4, col5 = st.columns([1,1 1])
+        with col4:
+            if st.button("רענן מידע", use_container_width=True):
+                st.cache_data.clear()
     with col2:
         st.image("Data/Logo.png", width=100)
 
