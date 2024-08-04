1536e1bf76071328c50c3eee75612bbf84458354
yuvaldror <yuval.dror@campus.technion.ac.il>
2024-08-04 15:53:32 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,71 +7,6 @@ import datetime as dt
 # Set up the page configuration
 st.set_page_config(page_title='פט קונקט', layout='wide', page_icon='Data/Logo.png')
 
-# Define CSS styles for the customized design
-st.markdown("""
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-        
-        body {
-            font-family: 'Roboto', sans-serif;
-            background-color: #DCD7C9; /* Light beige background */
-        }
-        .header {
-            text-align: center;
-            font-size: 2.5em;
-            margin-top: 20px;
-            color: #2C3639; /* Dark color for headers */
-        }
-        .subheader {
-            text-align: center;
-            font-size: 1.5em;
-            color: #3F4E4F; /* Dark grey color for subheaders */
-        }
-        .login-container {
-            max-width: 500px;
-            margin: auto;
-            padding: 20px;
-            border: 1px solid #ddd;
-            border-radius: 10px;
-            background-color: #ffffff; /* White background for the login container */
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        }
-        .record {
-            text-align: right;
-            padding: 10px;
-            border: 1px solid #ddd;
-            border-radius: 10px;
-            margin-bottom: 10px;
-            background-color: #ffffff; /* White background for records */
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        }
-        .refresh-btn {
-            display: flex;
-            justify-content: center;
-            margin: 20px 0;
-        }
-        .stButton > button {
-            color: #ffffff; /* White text for buttons */
-            background-color: #A27B5C; /* Accent color for buttons */
-            border-radius: 5px;
-            transition: background-color 0.3s, transform 0.3s;
-        }
-        .stButton > button:hover {
-            background-color: #8C6248; /* Darker accent color on hover */
-            transform: scale(1.05);
-        }
-        .stButton > button.logout {
-            background-color: red; /* Red color for logout button */
-            border-radius: 5px;
-            transition: background-color 0.3s, transform 0.3s;
-        }
-        .stButton > button.logout:hover {
-            background-color: #cc0000; /* Darker red on hover */
-            transform: scale(1.05);
-        }
-    </style>
-""", unsafe_allow_html=True)
-
 con1 = st.container()
 with con1:
     col1, col2 = st.columns([5, 1])
@@ -103,22 +38,22 @@ def login(username, password):
 
 # Function to show the login form and handle the login process
 def show_login_page():
-    st.markdown("<h1 class='header'>ברוך הבא לPetConnect</h1>", unsafe_allow_html=True)
-    st.markdown("<h2 class='subheader'>Please log in to access the system.</h2>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: right;'>PetConnectברוך הבא ל</h1>", unsafe_allow_html=True)
+    
+    st.subheader("Please log in to access the system.")
 
     # User input for login
-    with st.form("login_form", clear_on_submit=True):
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        submitted = st.form_submit_button("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-        if submitted:
-            if login(username, password):
-                st.session_state['logged_in'] = True
-                st.session_state['username'] = username
-                st.experimental_rerun()  # Refresh the page to update the content
-            else:
-                st.error("Invalid username or password")
+    # Login button
+    if st.button("Login"):
+        if login(username, password):
+            st.session_state['logged_in'] = True
+            st.session_state['username'] = username
+            st.experimental_rerun()  # Refresh the page to update the content
+        else:
+            st.error("Invalid username or password")
 
 # Function to show the home page
 def show_home_page():
@@ -129,17 +64,20 @@ def show_home_page():
         conn = st.connection("gsheets", type=GSheetsConnection, ttl=0.5)
         return conn.read(spreadsheet=url)
     
-    if 'seen_records' not in st.session_state:
-        st.session_state['seen_records'] = []
+    def save_data(df):
+        conn = st.connection("gsheets", type=GSheetsConnection, ttl=0.5)
+        # Debugging line
+        st.write("Attempting to save data to Google Sheets")
+        conn.write(df, spreadsheet=url)
 
-    st.markdown("<h1 class='header'>מסך עדכונים</h1>", unsafe_allow_html=True)
-
-    with st.container():
-        if st.button("רענן"):
-            st.cache_data.clear()
-            st.success("המידע עודכן!")
+    if st.button("רענן"):
+        st.cache_data.clear()
+        st.success("המידע עודכן!")
     
     df = fetch_data()
+    
+    # Debugging line
+    st.write("Data fetched from Google Sheets", df)
     
     # Clean up the column names
     df.columns = [col.strip() for col in df.columns]
@@ -169,30 +107,39 @@ def show_home_page():
     # Ensure the timestamp column is in datetime format
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
     
-    # Add a unique identifier to each record
-    df['Record ID'] = df.index
-    
+    # Add the 'seen' column if it doesn't exist and set to False where NaN
+    if 'seen' not in df.columns:
+        df['seen'] = False
+    else:
+        df['seen'] = df['seen'].fillna(False)
+
     # Filter the DataFrame to include only records from the past two days and unseen records
     two_days_ago = dt.datetime.now() - dt.timedelta(days=2)
-    recent_df = df[(df['Timestamp'] >= two_days_ago) & (~df['Record ID'].isin(st.session_state['seen_records']))]
+    recent_df = df[(df['Timestamp'] >= two_days_ago) & (~df['seen'])]
+    
+    # Set the title and subtitle
+    st.markdown("<h1 style='text-align: center;'>מסך עדכונים</h1>", unsafe_allow_html=True)
     
     # Display the number of records
     if len(recent_df) > 0:
-        st.markdown(f"<h2 class='subheader'>התקבלו {len(recent_df)} בקשות ביומיים האחרונים</h2>", unsafe_allow_html=True)
+        st.markdown(f"<h2 style='text-align: center;'>התקבלו {len(recent_df)} בקשות ביומיים האחרונים</h2>", unsafe_allow_html=True)
         
         # Display each record as text
         for i in range(len(recent_df)):
             st.markdown(f"""
-            <div class='record'>
+            <div style='text-align: right;'>
                 <p><b>שם:</b> {recent_df.iloc[i]['Full Name']}</p>
                 <p><b>כלב:</b> {recent_df.iloc[i]['Which dog are you interested in?']}</p>
                 <p><b>מידע נוסף:</b> {recent_df.iloc[i]['Additional information']}</p>
             </div>
+            <hr>
             """, unsafe_allow_html=True)
-            if st.checkbox("ראיתי", key=f"seen_{recent_df.iloc[i]['Record ID']}"):
-                st.session_state['seen_records'].append(recent_df.iloc[i]['Record ID'])
+            if st.checkbox("ראיתי", key=f"seen_{i}"):
+                df.loc[recent_df.index[i], 'seen'] = True
+
+        save_data(df)
     else:
-        st.markdown("<h2 class='subheader'>אין עדכונים חדשים!</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>אין עדכונים חדשים!</h2>", unsafe_allow_html=True)
 
 # Check if the user is logged in
 if 'logged_in' not in st.session_state:
@@ -200,7 +147,7 @@ if 'logged_in' not in st.session_state:
 
 # Main routing logic
 if st.session_state['logged_in']:
-    if st.sidebar.button("Log Out", key='logout', help='Log Out', css_class='logout'):
+    if st.sidebar.button("Log Out"):
         st.session_state['logged_in'] = False
         st.experimental_rerun()  # Refresh the page to update the content
     else:
