38a1c98949599b2b463d11d020f35d8439195b31
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-08-08 21:47:16 +0300

    Update 6Shopping List Generator.py

--- pages/6Shopping List Generator.py
+++ pages/6Shopping List Generator.py
@@ -12,45 +12,39 @@ dogs_file_path = "Data/Dogs.csv"
 dog_df = pd.read_csv(dogs_file_path, encoding='utf-8')
 dog_df = dog_df[dog_df['AdoptionStatus'] == 0]
 
-
 def show_shopping_list_page():
     if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
         st.error("לא ניתן לגשת לעמוד ללא התחברות")
         st.stop()
-    else:   
-        st.set_page_config(page_title='Shopping List', layout='wide')
-        
 
-        # Define the menu options
-        selected = option_menu(
-            menu_title="",  # Required
-            options=["צור רשימה לכלב", "הוסף מוצר"],  # Added new option for the table with scores
-            icons=["file", "search", "file", "upload", "table"],  # Optional
-            menu_icon="menu",  # Optional
-            default_index=0,  # Optional
-            orientation="horizontal",  # To place the menu in the center horizontally
-            styles={
-                "container": {"class": "option-menu-container"}}
-                        )
-        if selected == "צור רשימה לכלב":
-                col1, col2 = st.columns([3, 1])
-                with col1:
-                    dog = st.selectbox('בחר כלב:', dog_df['Name'].unique())
-                with col2:
-                    if st.button("צור רשימה"):
-                        with st.form("my_form"):
-                            create_list(dog)
-                            submitted = st.form_submit_button("Download")
-                            if submitted:
-                                st.write("downloading data")
-    
-        if selected == "הוסף מוצר":
-            st.write("not yet")
-    
-        # Sidebar logout button
-        if st.sidebar.button("Log Out"):
-            st.session_state['logged_in'] = False
-            st.experimental_rerun()
+    st.set_page_config(page_title='Shopping List', layout='wide')
+
+    # Define the menu options
+    selected = option_menu(
+        menu_title="",  # Required
+        options=["צור רשימה לכלב", "הוסף מוצר"],  # Added new option for the table with scores
+        icons=["file", "search", "file", "upload", "table"],  # Optional
+        menu_icon="menu",  # Optional
+        default_index=0,  # Optional
+        orientation="horizontal",  # To place the menu in the center horizontally
+        styles={
+            "container": {"class": "option-menu-container"}}
+                    )
+    if selected == "צור רשימה לכלב":
+            col1, col2 = st.columns([3, 1])
+            with col1:
+                dog = st.selectbox('בחר כלב:', dog_df['Name'].unique())
+            with col2:
+                if st.button("צור רשימה"):
+                    create_list(dog)
+
+    if selected == "הוסף מוצר":
+        st.write("not yet")
+
+    # Sidebar logout button
+    if st.sidebar.button("Log Out"):
+        st.session_state['logged_in'] = False
+        st.experimental_rerun()
     
     def create_list(dog):
         df = dog_df.loc[dog_df['Name'] == dog].reset_index()
