e0553fbf8a20ee4f4589f1980b32ab06201ab59f
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-08-08 21:31:11 +0300

    Update 1Adopters.py

--- pages/1Adopters.py
+++ pages/1Adopters.py
@@ -53,6 +53,7 @@ adopter_df_hebrew = load_adopters_data()
         
 def show_adopters_page():
     st.set_page_config(page_title='Adopters', layout='wide')
+    
     if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
         st.error("לא ניתן לגשת לעמוד ללא התחברות")
         st.stop()
@@ -373,9 +374,9 @@ def show_adopters_page():
                 else:
                     st.error('אין שם לקובץ ')
 
-# Sidebar logout button
-if st.sidebar.button("Log Out"):
-    st.session_state['logged_in'] = False
-    st.experimental_rerun()
+    # Sidebar logout button
+    if st.sidebar.button("Log Out"):
+        st.session_state['logged_in'] = False
+        st.experimental_rerun()
 
 show_adopters_page()
