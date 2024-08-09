f046b4f1a7e1e1661b668510b3100e1edb788710
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-08-09 13:10:35 +0300

    Update 6Shopping List Generator.py

--- pages/6Shopping List Generator.py
+++ pages/6Shopping List Generator.py
@@ -18,15 +18,85 @@ def show_shopping_list_page():
         st.stop()
 
     st.set_page_config(page_title='Shopping List', layout='wide')
-    con1 = st.container()
-    with con1:
-        col1, col2 = st.columns([4, 1])  # Adjust column ratios as needed
+    with st.container():
+        col4, col1, col2 = st.columns([1, 10, 1])
         with col1:
-            st.markdown('<h1 class="rtl">Foster Homes</h1>', unsafe_allow_html=True)
+            st.markdown("<h1 style='text-align: center;'>רשימת קניות</h1>", unsafe_allow_html=True)
         with col2:
             st.image("Data/Logo.png", width=100)
 
+        url = "https://docs.google.com/spreadsheets/d/1u37tuMp9TI2QT6yyT0fjpgn7wEGlXvYYKakARSGRqs4/edit?usp=sharing"
+        # Custom CSS to center-align the option menu
+    st.markdown(
+        """
+        <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
     
+    .stButton > button {
+        color: #ffffff; /* White text for buttons */
+        background-color: #30475E; /* Dark blue color for buttons */
+        border-radius: 5px;
+        padding: 10px 20px;
+        transition: background-color 0.3s, transform 0.3s;
+        font-size: 1em;
+    }
+    .stButton > button:hover {
+        background-color: #25394C; /* Darker blue on hover */
+        transform: scale(1.05);
+    }
+    .stButton > button.logout {
+        background-color: #F05454; /* Red color for logout button */
+        border-radius: 5px;
+        transition: background-color 0.3s, transform 0.3s;
+        padding: 10px 20px;
+        font-size: 1em;
+    }
+    .stButton > button.logout:hover {
+        background-color: #C74444; /* Darker red on hover */
+        transform: scale(1.05);
+    }
+    .icon-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .icon-button img {
+        margin-right: 5px;
+    }
+    .option-menu-container {
+        display: flex;
+        justify-content: center;
+    }
+    .dataframe-container {
+        background-color: #ffffff; /* White background for dataframe */
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        padding: 10px;
+    }
+    .file-upload-container {
+        background-color: #ffffff; /* White background for file upload */
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+        margin-top: 20px;
+    }
+    .stDownloadButton > button {
+        color: #ffffff; /* White text for download buttons */
+        background-color: #30475E; /* Dark blue color for download buttons */
+        border-radius: 5px;
+        padding: 10px 20px;
+        transition: background-color 0.3s, transform 0.3s;
+        font-size: 1em;
+    }
+    .stDownloadButton > button:hover {
+        background-color: #25394C; /* Darker blue on hover */
+        transform: scale(1.05);
+    }
+    </style>
+
+        """,
+        unsafe_allow_html=True
+    )
     # Define the menu options
     selected = option_menu(
         menu_title="",  # Required
@@ -39,12 +109,25 @@ def show_shopping_list_page():
             "container": {"class": "option-menu-container"}}
                     )
     if selected == "צור רשימה לכלב":
-            col1, col2 = st.columns([3, 1])
-            with col1:
-                dog = st.selectbox('בחר כלב:', dog_df['Name'].unique())
-            with col2:
-                if st.button("צור רשימה"):
-                    create_list(dog)
+        col3, col1, col2, col4 = st.columns([0.5, 4.5, 1, 0.5])
+        with col1:
+            st.markdown(
+                """
+                <style>
+                [data-baseweb="select"] {
+                    margin-top: -40px;
+                }
+                </style>
+                """,
+                unsafe_allow_html=True,
+            )
+            dog = st.selectbox(label='',options=dog_df['Name'].unique(),index=None, placeholder="בחר כלב")
+        with col2:
+            if st.button("צור רשימה"):
+                st.session_state['list']=True
+            
+    if st.session_state['list']:
+        create_list(dog)
 
     if selected == "הוסף מוצר":
         st.write("not yet")
@@ -55,26 +138,27 @@ def show_shopping_list_page():
         st.switch_page("Home.py")
         st.experimental_rerun()
 
-    def create_list(dog):
-        df = dog_df.loc[dog_df['Name'] == dog].reset_index()
-        categories = items_df['Product Category'].unique()
-        sl = items_df.iloc[:0,:].copy()
-        flag = False
-        if not (df["Size"][0]=='XS' or df["Size"][0]=='S' or df["Size"][0]=='M' or df["Size"][0]=='L' or df["Size"][0]=='XL'):
-            st.warning('Dog has NO size!')
-            flag = True
-        for c in categories:
-            if c=="גורים":
-                if df["Age"][0]<12:
-                    category_products = items_df[items_df['Product Category'] == c]
-                    sl = pd.concat([sl, category_products], ignore_index=True) 
-            else:    
-                if not flag:
-                    category_products = items_df[items_df['Product Category'] == c]
-                    sl = pd.concat([sl, category_products[category_products['Dog Size'] == df["Size"][0]]], ignore_index=True) 
-        
-        sl['new_column'] = True
-        sl = st.data_editor(sl) 
+def create_list(dog):
+    df = dog_df.loc[dog_df['Name'] == dog].reset_index()
+    categories = items_df['Product Category'].unique()
+    sl = items_df.iloc[:0,:].copy()
+    flag = False
+    if not (df["Size"][0]=='XS' or df["Size"][0]=='S' or df["Size"][0]=='M' or df["Size"][0]=='L' or df["Size"][0]=='XL'):
+        st.warning('Dog has NO size!')
+        st.stop
+        flag = True
+    for c in categories:
+        if c=="גורים":
+            if df["Age"][0]<12:
+                category_products = items_df[items_df['Product Category'] == c]
+                sl = pd.concat([sl, category_products], ignore_index=True) 
+        else:    
+            if not flag:
+                category_products = items_df[items_df['Product Category'] == c]
+                sl = pd.concat([sl, category_products[category_products['Dog Size'] == df["Size"][0]]], ignore_index=True) 
+    
+    sl['new_column'] = True
+    sl = st.data_editor(sl) 
         
     
 show_shopping_list_page()
