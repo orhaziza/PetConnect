98ebde9680ed4c4ac48b9e9378c46ebf55990615
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-08-09 13:10:24 +0300

    Update 5Data Analysis.py

--- pages/5Data Analysis.py
+++ pages/5Data Analysis.py
@@ -8,7 +8,7 @@ import numpy as np
 from streamlit_gsheets import GSheetsConnection
 import altair as alt
 
-url = "https://docs.google.com/spreadsheets/d/1u37tuMp9TI2QT6yyT0fjpgn7wEGlXvYYKakARSGRqs4/edit?usp=sharing"
+data_url = "https://docs.google.com/spreadsheets/d/1u37tuMp9TI2QT6yyT0fjpgn7wEGlXvYYKakARSGRqs4/edit?usp=sharing"
 
 def show_data_analysis_page():
     if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
@@ -20,15 +20,89 @@ def show_data_analysis_page():
     with st.container():
         col1, col2 = st.columns([15, 1])
         with col1:
-            st.markdown("<h1 style='text-align: center;'>Data Analysis Page</h1>", unsafe_allow_html=True)
-            st.markdown("<h3 style='text-align: center;'>כאן תוכלו לצפות בויזואליזציות על בסיס הנתונים שנאספו עד כה</h3>", unsafe_allow_html=True)
+            st.markdown("<h1 style='text-align: center;'>ניתוח נתונים</h1>", unsafe_allow_html=True)
+            st.markdown("<h4 style='text-align: center;'>כאן תוכלו לצפות בויזואליזציות על בסיס הנתונים שנאספו עד כה</h4>", unsafe_allow_html=True)
             col3, col4, col5 = st.columns([2,1,2])
             with col4:
-                if st.button("רענן מידע", use_container_width=True):
+                if st.button("רענן", use_container_width=True):
+                    st.success("המידע התעדכן בהצלחה")
                     st.cache_data.clear()
         with col2:
             st.image("Data/Logo.png", width=100)
 
+    url = "https://docs.google.com/spreadsheets/d/1u37tuMp9TI2QT6yyT0fjpgn7wEGlXvYYKakARSGRqs4/edit?usp=sharing"
+        # Custom CSS to center-align the option menu
+    st.markdown(
+        """
+        <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+    
+    .stButton > button {
+        color: #ffffff; /* White text for buttons */
+        background-color: #30475E; /* Dark blue color for buttons */
+        border-radius: 5px;
+        padding: 10px 20px;
+        transition: background-color 0.3s, transform 0.3s;
+        font-size: 1em;
+    }
+    .stButton > button:hover {
+        background-color: #25394C; /* Darker blue on hover */
+        transform: scale(1.05);
+    }
+    .stButton > button.logout {
+        background-color: #F05454; /* Red color for logout button */
+        border-radius: 5px;
+        transition: background-color 0.3s, transform 0.3s;
+        padding: 10px 20px;
+        font-size: 1em;
+    }
+    .stButton > button.logout:hover {
+        background-color: #C74444; /* Darker red on hover */
+        transform: scale(1.05);
+    }
+    .icon-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .icon-button img {
+        margin-right: 5px;
+    }
+    .option-menu-container {
+        display: flex;
+        justify-content: center;
+    }
+    .dataframe-container {
+        background-color: #ffffff; /* White background for dataframe */
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        padding: 10px;
+    }
+    .file-upload-container {
+        background-color: #ffffff; /* White background for file upload */
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+        margin-top: 20px;
+    }
+    .stDownloadButton > button {
+        color: #ffffff; /* White text for download buttons */
+        background-color: #30475E; /* Dark blue color for download buttons */
+        border-radius: 5px;
+        padding: 10px 20px;
+        transition: background-color 0.3s, transform 0.3s;
+        font-size: 1em;
+    }
+    .stDownloadButton > button:hover {
+        background-color: #25394C; /* Darker blue on hover */
+        transform: scale(1.05);
+    }
+    </style>
+
+        """,
+        unsafe_allow_html=True
+    )
+   
     adopter_file_path = "Data/Adopters.csv"
     if not os.path.exists(adopter_file_path):
         st.error("The adopter file does not exist.")
@@ -50,22 +124,22 @@ def show_data_analysis_page():
     with st.container():
         col1 , col2 = st.columns([1,1], gap="small")
         with col1:
-            st.markdown("<h6 style='text-align: right;'>התפלגות בתי אומנה</h6>", unsafe_allow_html=True)
+            st.markdown("<h5 style='text-align: center;'>התפלגות בתי אומנה</h5>", unsafe_allow_html=True)
             plot_Fosters(Foster_Home_df)
         with col2:
-            st.markdown("<h6 style='text-align: right;'>התפלגות הכלבים בעמותה</h6>", unsafe_allow_html=True)
+            st.markdown("<h5 style='text-align: center;'>התפלגות הכלבים בעמותה</h5>", unsafe_allow_html=True)
             plot_Dogs(dogs_df)
     
     with st.container():
         col1 , col2 , col3 = st.columns([1,1,1], gap="small")
         with col1:
-            st.markdown("<h6 style='text-align: right;'>התפלגות בקשות אימוץ לפי פלטפורמת פרסום</h6>", unsafe_allow_html=True)
+            st.markdown("<h5 style='text-align: center;'>התפלגות בקשות אימוץ לפי פלטפורמת פרסום</h5>", unsafe_allow_html=True)
             plot_Applications(fetch_data())
         with col2:
-            st.markdown("<h6 style='text-align: right;'>בקשות אימוץ לאורך זמן</h6>", unsafe_allow_html=True)
+            st.markdown("<h5 style='text-align: center;'>בקשות אימוץ לאורך זמן</h5>", unsafe_allow_html=True)
             plot_Applications_Flow(fetch_data())
         with col3:
-            st.markdown("<h6 style='text-align: right;'>בקשות אימוץ לפי יום בשבוע</h6>", unsafe_allow_html=True)
+            st.markdown("<h5 style='text-align: center;'>בקשות אימוץ לפי יום בשבוע</h5>", unsafe_allow_html=True)
             plot_Applications_by_WkDay(fetch_data())
 
     # Sidebar logout button
@@ -76,7 +150,7 @@ def show_data_analysis_page():
 @st.cache_data()
 def fetch_data():
     conn = st.connection("gsheets", type=GSheetsConnection)
-    return conn.read(spreadsheet=url)
+    return conn.read(spreadsheet=data_url)
 
 def plot_Applications(application_df):
     platform_counts = application_df.iloc[:, 1].value_counts()
@@ -91,35 +165,87 @@ def plot_Applications(application_df):
 
 def plot_Fosters(Foster_Home_df):
     lst = ['HouseSize','Backyard','NearDogPark','HouseMembers','AvailabilityAtHome','ChildrenFriendly','AnimalFriendly','MaximumCapacity','AllowedAtProperty','Allergies','IsMobile','EnergyLevel','PastFosters','PastExperience']
-    characteristic = st.selectbox('בחר מאפיין', lst)
+    st.markdown(
+            """
+            <style>
+            .stSelectbox label {
+                display: block;
+                text-align: center;
+            }
+            </style>
+            """, 
+            unsafe_allow_html=True
+        )
+
+    characteristic = st.selectbox(':בחר מאפיין', lst)
     if Foster_Home_df[characteristic].dtype == 'bool':
         distribution = Foster_Home_df[characteristic].astype(str).value_counts()
-        st.subheader(f'Distribution of foster homes by {characteristic.capitalize()}')
+        # st.subheader(f'Distribution of foster homes by {characteristic.capitalize()}')
         st.bar_chart(distribution)
     else:
         distribution = Foster_Home_df[characteristic].value_counts()
-        st.subheader(f'Distribution of foster homes by {characteristic.capitalize()}')
+        # st.subheader(f'Distribution of foster homes by {characteristic.capitalize()}')
         st.bar_chart(distribution)
 
 def plot_Dogs(dogs_df):
     lst = ['Age','Breed','Weight','Size','Gender','RescueDate','Rabies_Done','Hexagonal_1','Hexagonal_2','Hexagonal_3','Hexagonal_Done','Spayed','De-worm','ChildrenFriendly','AnimalFriendly','HealthStatus','EnergyLevel','PhotographStatus','AdoptionStatus','AdopterID','PottyTrained']
-    characteristic = st.selectbox('בחר מאפיין', lst)
+
+    st.markdown(
+        """
+        <style>
+        .stSelectbox label {
+            display: block;
+            text-align: center;
+        }
+        </style>
+        """, 
+        unsafe_allow_html=True
+    )
+    characteristic = st.selectbox(':בחר מאפיין', lst)
     
     if dogs_df[characteristic].dtype == 'bool':
       # Convert boolean to string
         distribution = dogs_df[characteristic].astype(str).value_counts()
-        st.subheader(f'Distribution of Dogs by {characteristic.capitalize()}')
+        # st.subheader(f'Distribution of Dogs by {characteristic.capitalize()}')
         st.bar_chart(distribution)
     else:
         distribution = dogs_df[characteristic].value_counts()
-        st.subheader(f'Distribution of Dogs by {characteristic.capitalize()}')
+        # st.subheader(f'Distribution of Dogs by {characteristic.capitalize()}')
         st.bar_chart(distribution)
 
 def plot_Applications_Flow(application_df):
     df = application_df
     df["חותמת זמן"] = pd.to_datetime(application_df["חותמת זמן"], yearfirst=True, format='%d/%m/%Y %H:%M:%S')
 
-    view = st.radio("בחר", ("שבועי", "חודשי"))
+    st.markdown(
+        """
+        <style>
+        .stRadio {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start; /* Align the label and options to the start (left) */
+            direction: rtl; /* Set the direction to RTL to handle Hebrew text correctly */
+        }
+        .stRadio > label {
+            margin-left: 100px;  /* Add space between the label and the options */
+            font-weight: bold;  /* Optional: Make the label bold */
+            white-space: nowrap; /* Prevent the label from wrapping to the next line */
+        }
+        .stRadio div {
+            display: flex;
+            flex-direction: row-reverse; /* Display the options to the left of the label */
+        }
+        .stRadio div label {
+            margin-left: 15px; /* Add space between the options */
+            display: flex;
+            align-items: center;
+        }
+        </style>
+        """, 
+        unsafe_allow_html=True
+    )
+
+    view = st.radio("בחר:", ("שבועי", "חודשי"))
 
     if view == "שבועי":
         df["Period"] = df["חותמת זמן"].dt.to_period('W').apply(lambda r: r.start_time)
