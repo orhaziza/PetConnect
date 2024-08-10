ec89b0dc0adef219ca99b5d376f373950150ce37
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-08-10 13:57:31 +0300

    Update 2כלבים.py

--- pages/2כלבים.py
+++ pages/2כלבים.py
@@ -136,6 +136,7 @@ def show_dogs_page():
         menu_icon="menu",  # Optional
         default_index=4,  # Optional
         orientation="horizontal"  # To place the menu in the center horizontally
+        styles=background.styles,
     )
 
     images_folder = "DogsPhotos"
