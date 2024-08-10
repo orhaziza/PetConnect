0e80c9d0e84a47cfb1c38798f873cce8642c4db4
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-08-10 13:57:48 +0300

    Update 3בתי אומנה.py

--- pages/3בתי אומנה.py
+++ pages/3בתי אומנה.py
@@ -101,6 +101,7 @@ def show_foster_homes_page():
         menu_icon="menu",  # Optional
         default_index=3,  # Optional
         orientation="horizontal",  # To place the menu in the center horizontally
+        styles=background.styles,
     )
 
     # Translate column names
