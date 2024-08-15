266e85c62def647091afbd8772f77f17f3a6935d
NourEldeneShannan <127997646+NourEldeneShannan@users.noreply.github.com>
2024-08-15 16:38:24 +0300

    Update 3בתי אומנה.py

--- pages/3בתי אומנה.py
+++ pages/3בתי אומנה.py
@@ -210,7 +210,7 @@ def show_foster_homes_page():
             new_foster_home_df = pd.DataFrame([new_foster_home])
 
             # Define the CSV file path (modify this path as needed)
-            csv_file_path = 'foster_home_data.csv'
+            csv_file_path = 'Data/FosterHome.csv'
 
             # Save the new foster home to the CSV
             try:
@@ -219,7 +219,7 @@ def show_foster_homes_page():
                 st.balloons()
                 st.print("XXXX")
                 # Ensure the file is added to Git
-                os.system(f'git add {Data/FosterHome.csv}')
+                os.system(f'git add {csv_file_path}')
                 os.system('git commit -m "Update foster home data with a new entry"')
                 os.system('git push')
                 
