1e2d73be0ab54034c34c36a209ac17be84a193fc
yuvaldror <yuval.dror@campus.technion.ac.il>
2024-08-15 13:04:19 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,8 +8,10 @@ import background
 # Set up the page configuration
 st.set_page_config(page_title='פט קונקט', layout='wide', page_icon='Data/Logo.png')
 
+
 background.add_bg_from_local('static/background3.png')
 background.load_css('styles.css')
+
 
 # Function to hash the password using SHA-256
 def hash_password(password):
@@ -68,6 +70,7 @@ def show_login_page():
 # Function to show the home page
 def show_home_page():
     url = "https://docs.google.com/spreadsheets/d/1u37tuMp9TI2QT6yyT0fjpgn7wEGlXvYYKakARSGRqs4/edit?usp=sharing"
+        # Custom CSS to center-align the option menu
     
     @st.cache_data()
     def fetch_data():
@@ -98,8 +101,7 @@ def show_home_page():
         'ניסיון עם בעלי חיים?': 'Experience with animals?',
         'האם יש בעלי חיים נוספים בבית': 'Do you have other pets?',
         'זמינות': 'Availability',
-        'במידה ויש, אילו?': 'If any, which?',
-        'Seen': 'Seen'  # Add this column to the mapping if needed
+        'במידה ויש, אילו?': 'If any, which?'
     }
 
     # Rename the columns in the DataFrame
@@ -108,16 +110,14 @@ def show_home_page():
     # Ensure the timestamp column is in datetime format
     df['Timestamp'] = pd.to_datetime(df['Timestamp'], format="%d/%m/%Y %H:%M:%S")
     
-    # Filter out seen records
-    unseen_df = df[df['Seen'] != 1]
-
     # Add a unique identifier to each record
-    unseen_df['Record ID'] = unseen_df.index
+    df['Record ID'] = df.index
     
-    # Filter the DataFrame to include only records from the past two days
+    # Filter the DataFrame to include only records from the past two days and unseen records
     two_days_ago = dt.datetime.now() - dt.timedelta(days=2)
-    recent_df = unseen_df[unseen_df['Timestamp'] >= two_days_ago]
+    recent_df = df[(df['Timestamp'] >= two_days_ago) & (~df['Record ID'].isin(st.session_state['seen_records']))]
     
+
     with st.container():
         col4, col1, col2 = st.columns([1, 10, 1])
         with col1:
@@ -136,6 +136,7 @@ def show_home_page():
     if len(recent_df) > 0:
         st.markdown(f"<h3 style='text-align: center;'>התקבלו {len(recent_df)} בקשות ביומיים האחרונים</h3>", unsafe_allow_html=True)
         
+# Display each record as text
         for i in range(len(recent_df)):
             phone_number = str(int(recent_df.iloc[i]['Phone number'])).zfill(10)
             formatted_phone_number = f"{phone_number[:3]}-{phone_number[3:]}"
@@ -148,26 +149,17 @@ def show_home_page():
 </div>
 """, unsafe_allow_html=True)
 
+    # Create a container with columns for button alignment
             col1, col2, col3 = st.columns([1, 0.2, 0.2])  # Adjust the ratios as needed
             with col1:
                 if st.button(f"סמן כראיתי", key=f"seen_button_{i}"):  # Assign a unique key for each button
-                    # Calculate the correct row number for Google Sheets
-                    record_id = recent_df.iloc[i]['Record ID']
-                    sheet_row = record_id + 2  # Assuming the first row is the header
-                    st.write(f"Updating Seen column for Record ID: {record_id} at Google Sheets row: {sheet_row}")  # Debugging output
-                    
-                    # Update the "Seen" column in the DataFrame
-                    df.at[record_id, 'Seen'] = 1
-                    
-                    # Update the data in Google Sheets
-                    conn = st.connection("gsheets", type=GSheetsConnection)
-                    conn.update_cell(f'P{sheet_row}', 1)  # 'Seen' is in column P
-                    
+                    st.session_state['seen_records'].append(recent_df.iloc[i]['Record ID'])
                     st.experimental_rerun()  # Optionally rerun to immediately reflect the update
-        
         st.markdown("<hr>", unsafe_allow_html=True)
     else:
         st.markdown("<h2 style='text-align: center;'>אין עדכונים חדשים!</h2>", unsafe_allow_html=True)
+
+
 
 # Check if the user is logged in
 if 'logged_in' not in st.session_state:
