933b86a3440add3c4dda64f6344946e4b9fe1442
orhaziza <82028972+orhaziza@users.noreply.github.com>
2024-09-08 17:47:41 +0300

    Update 6רשימת קניות.py

--- pages/6רשימת קניות.py
+++ pages/6רשימת קניות.py
@@ -36,12 +36,6 @@ hebrew_columns_items = {
 items_df = items_df.rename(columns=dict(zip(items_df.columns, [hebrew_columns_items.get(col, col) for col in items_df.columns])))
 items_df = items_df.iloc[:, ::-1]
 
-def generate_pdf(text_content):
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=12)
-    pdf.multi_cell(0, 10, text_content)
-    return pdf.output(dest='S').encode('latin1')
  
 def show_shopping_list_page():    
     background.add_bg_from_local('./static/background3.png')
