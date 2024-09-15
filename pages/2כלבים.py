0aaa1819e0cd721a4cd88e37ec1beb9702cb6026
NourEldeneShannan <127997646+NourEldeneShannan@users.noreply.github.com>
2024-09-15 11:27:00 +0300

    Update 2כלבים.py

--- pages/2כלבים.py
+++ pages/2כלבים.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+from streamlit_option_menu import option_menu
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 import numpy as np
