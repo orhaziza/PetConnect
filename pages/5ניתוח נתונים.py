37ec4979bb5b0b39f9f0304df1bcc8d6598b047a
NourEldeneShannan <127997646+NourEldeneShannan@users.noreply.github.com>
2024-09-15 15:29:01 +0300

    Update 5ניתוח נתונים.py

--- pages/5ניתוח נתונים.py
+++ pages/5ניתוח נתונים.py
@@ -297,10 +297,9 @@ def plot_Applications(application_df):
     st.pyplot(fig)
 
 def plot_Fosters(Foster_Home_df):
-   lst = ['מזהה בית אומנה', 'שם בית אומנה', 'כתובת', 'גודל הבית', 'פרטי קשר', 'חצר', 'NearDogPark', 
+    lst = ['מזהה בית אומנה', 'שם בית אומנה', 'כתובת', 'גודל הבית', 'פרטי קשר', 'חצר', 'NearDogPark', 
        'חברי בית', 'זמינות בבית', 'ידידותי לילדים', 'ידידותי לכלבים', 'קיבולת מקסימלית', 
        'AllowedAtProperty', 'Allergies', 'נייד', 'רמת אנרגיה', 'PastFosters', 'PastExperience', 'Documents']
-
     
     characteristic = st.selectbox(':בחר מאפיין', lst)
     distribution = Foster_Home_df[characteristic].value_counts()
